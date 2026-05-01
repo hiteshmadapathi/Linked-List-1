14f292af49bbf32381fd71563fc54e417c8ee3e5
M.Hitesh <54840773+hiteshmadapathi@users.noreply.github.com>
2026-05-01 14:57:37 -0700

    Create Problem1.py

---
+++ Problem1.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Time Complexity --> O(n)
+# Space Complexity --> O(1)
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr is not None:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
